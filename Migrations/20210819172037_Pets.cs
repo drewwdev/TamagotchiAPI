4c5c69cafbd46f3e2230e26888a233cc4e32a901
Drew Wilson <jankyy93@gmail.com>
2021-08-19 13:32:15 -0400

    Models created

---
+++ Migrations/20210819172037_Pets.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TamagotchiAPI.Migrations
+{
+    public partial class Pets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
